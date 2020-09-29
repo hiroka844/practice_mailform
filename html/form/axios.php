65faf894f2081891a86e7ba759a07ff2224d6787
hiroka.yonebayashi <hiroka.yonebayashi@gpol.co.jp>
2020-09-29 14:35:23 +0900

    [update]9.29発表分

--- html/form/axios.php
+++ html/form/axios.php
@@ -8,9 +8,7 @@
     if(!isset($_POST['name_mei']) || empty($_POST['name_mei'])){
         $error['name_mei'] = '名を入力してください';
     }
-    var_dump(77);
     echo json_encode($error,JSON_UNESCAPED_UNICODE);
     exit();
-
 ?>
 
